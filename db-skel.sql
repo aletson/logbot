182b4d0feb5b977dcd62a85134b440f23e973010
Andrew Letson <velinath@users.noreply.github.com>
2017-11-09 08:00:19 -0500

    Update db-skel.sql

--- db-skel.sql
+++ db-skel.sql
@@ -2,5 +2,6 @@ CREATE TABLE messages(
   id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   message TEXT,
   author_id TEXT,
-  channel_id TEXT
+  channel_id TEXT,
+  created_on DATETIME DEFAULT CURRENT_TIMESTAMP
 );
